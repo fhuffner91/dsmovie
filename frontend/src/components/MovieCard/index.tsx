83a42fcaf305fe1ea671b9653a44eaec65c81824
Felipe Huffner <felipeqh.1991@gmail.com>
2022-01-11 17:30:57 -0300

    Navigation buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,4 +1,5 @@
 import MovieScore from "components/MovieScore";
+import { Link } from "react-router-dom";
 
 
 
@@ -21,7 +22,11 @@ function MovieCard() {
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
         <MovieScore />
+
+        <Link to={`/form/${movie.id}`}>
         <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+        </Link>
+        
       </div>
     </div>
   );
