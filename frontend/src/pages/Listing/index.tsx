bcd842279706170fc353a8c2e72384c1ee242f7e
Felipe Huffner <felipeqh.1991@gmail.com>
2022-01-14 17:29:15 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,63 +1,49 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import MovieCard from 'components/MovieCard'
+import Pagination from 'components/Pagination'
+import { BASE_URL } from 'utils/requests'
+import { MoviePage } from 'types/movie'
 
-import axios from "axios";
-import { useEffect, useState } from "react";
-import MovieCard from "components/MovieCard";
-import Pagination from "components/Pagination";
-import { BASE_URL } from "utils/requests";
-import { MoviePage } from "types/movie";
+function Listing() {
+  const [pageNumber, setPageNumber] = useState(0)
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
+  })
 
-function Listing(){
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+      .then((response) => {
+        const data = response.data as MoviePage
+        setPage(data)
+      })
+  }, [pageNumber])
 
-    const [pageNumber, setPageNumber] = useState(0);
-
-    useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=1`)
-        .then(response => {
-            const data = response.data as MoviePage;
-            console.log(data)
-            setPageNumber(data.number);
-        });
-
-    },[]);
-   
-    
-   
-    
-    
-
-    
-
-
-    return (
+  return (
     <>
+      <Pagination />
 
-    <p>{pageNumber}</p>
-
-        <Pagination />
-
-
-        <div className="container">
-
-          <div className="row">
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard />
+      <div className="container">
+        <div className="row">
+          {page.content.map((movie) => (
+          <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard movie={movie} />
             </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard />
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard />
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard />
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard />
-            </div>
-         </div>
+          )
+          )}
         </div>
+      </div>
     </>
-    );
+  )
 }
 
-export default Listing;
+export default Listing
